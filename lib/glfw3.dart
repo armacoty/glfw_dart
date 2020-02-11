4fbd9c1283a3fa7d1466188c39a2ffd34447c1c3
Vinogradov Alexander <astronom_35@mail.ru>
2020-02-11 20:15:05 +0300

    Moved all to src/bind + started writing docs

--- lib/glfw3.dart
+++ lib/glfw3.dart
@@ -1,10 +1,18 @@
+/// Bindings for glfw3 library
+/// 
+/// Docs for glfw: [glfw.org/docs/latest/](https://www.glfw.org/docs/latest/)
+/// 
+/// But it is worth considering the specifics of the FFI.
+/// Docs for FFI:
+/// * [api.dart.dev/stable/2.7.1/dart-ffi/dart-ffi-library.html](https://api.dart.dev/stable/2.7.1/dart-ffi/dart-ffi-library.html)
+/// * [pub.dev/packages/ffi](https://pub.dev/packages/ffi)
 library glfw3;
 
-import 'src/functions.dart';
+import 'src/bind/functions.dart';
 
-export 'src/callback_typedefs.dart';
-export 'src/constants.dart';
-export 'src/functions.dart';
-export 'src/structures.dart';
+export 'src/bind/callback_typedefs.dart';
+export 'src/bind/constants.dart';
+export 'src/bind/functions.dart';
+export 'src/bind/structures.dart';
 
 Glfw glfw = Glfw();
