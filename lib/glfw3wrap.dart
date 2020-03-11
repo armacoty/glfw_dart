8c47cd8212a8b62b177e80e390dc2e5338998901
Vinogradov Alexander <astronom_35@mail.ru>
2020-03-11 21:40:58 +0300

    wrapper development is canceled

--- lib/glfw3wrap.dart
+++
@@ -1,8 +0,0 @@
-/// High level wrapper for glfw3 ffi bindings.
-///
-/// __Notice: functionality will be implemented in 0.1.0__
-///
-/// To use, import `package:glfw_dart/glfw3wrap.dart`.
-library glfw3wrap;
-
-// TODO
