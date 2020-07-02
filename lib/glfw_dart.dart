f8531d3c4f9f220020b96b873a103e36e5a5243e
Vinogradov Alexander <astronom_35@mail.ru>
2020-07-02 21:02:07 +0300

    manual initialization

---
+++ lib/glfw_dart.dart
@@ -0,0 +1,14 @@
+/// Bindings for glfw3 library
+///
+/// Docs for glfw: [glfw.org/docs/latest/](https://www.glfw.org/docs/latest/)
+///
+/// But it is worth considering the specifics of the FFI.
+/// Docs for FFI:
+/// * [api.dart.dev/stable/2.7.1/dart-ffi/dart-ffi-library.html](https://api.dart.dev/stable/2.7.1/dart-ffi/dart-ffi-library.html)
+/// * [pub.dev/packages/ffi](https://pub.dev/packages/ffi)
+library glfw_dart;
+
+export 'src/callback_typedefs.dart';
+export 'src/constants.dart';
+export 'src/functions.dart';
+export 'src/structures.dart';
